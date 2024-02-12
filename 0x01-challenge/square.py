76c582a9b72dcfc43ceb913268d3d68078eb926f
AAEmara <abdelrahmanemara18@gmail.com>
2024-02-11 17:26:49 -0800

    0x01: Task1 Python Script - Fixing Initialization.

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,8 +10,12 @@ class square():
 
     def __init__(self, *args, **kwargs):
         """Intializing the object with the given input"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if len(args):
+            self.width = args[0]
+            self.height = self.width
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def area_of_my_square(self):
         """Calculates the area of the square."""
@@ -28,7 +32,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(12, 9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
