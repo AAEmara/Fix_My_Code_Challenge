dfa993d2bced304d32e343190cea88a6bbe7a7e5
AAEmara <abdelrahmanemara18@gmail.com>
2024-02-11 17:53:28 -0800

    0x01: Task1 Python Script - Making it Simple.
    Just refixing the original Python Script
    through adjusting the docstrings and the
    area calculation.

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""square module for representing the square class."""
+"""square module with Square Class."""
 
 
 class square():
-    """square class."""
+    """Square class with some calculation features."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Intializing the object with the given input"""
+        """Initializing the instance attributes."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Calculates the area of the square."""
+        """ Area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """Calculates the perimeter of the square."""
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """String representation of the sqare object."""
+        """ String Representation for the Square Class. """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Square Class Testing """
+    """ Square Class Testing. """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
